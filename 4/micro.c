8b202e86ea52585131e8cb50030feae2620b4abf
Aleksandr Safronov <asafrono@student.42lisboa.com>
2025-05-11 18:17:49 +0100

    4

--- 4/micro.c
+++ 4/micro.c
@@ -6,7 +6,7 @@
 /*   By: asafrono <asafrono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 16:32:46 by asafrono          #+#    #+#             */
-/*   Updated: 2025/05/11 16:43:08 by asafrono         ###   ########.fr       */
+/*   Updated: 2025/05/11 18:17:06 by asafrono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,85 @@ void ft_fatal_error(void)
 	exit(1);
 }
 
+void ft_execute(char **argv, int i, int tmp_fd, char **env)
+{
+	argv[i] = NULL;
+	if (dup2(tmp_fd, 0) == -1)
+		ft_fatal_error();
+	close(tmp_fd);
+	execve(argv[0], argv, env);
+	ft_putstr_fd2("error: cannot execute ", argv[0]);
+	exit(1);
+}
 
-
-int main (int argc, char *argv[], char *env[])
+int main (int argc, char **argv, char **env)
 {
 	int		i;
 	int 	fd[2];
 	int 	tmp_fd;
 	pid_t 	pid;
 
+	(void)argc;
+	i = 0;
+	tmp_fd = dup(0);
+	if (tmp_fd == -1)
+		ft_fatal_error();
+	
+	while (argv[i] && argv[i+1])
+	{
+		argv = &argv[i+1];
+		i = 0;
 
-
+		while(argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
+			i++;
+		
+		if (strcmp(argv[0], "cd") == 0)
+		{
+			if (i != 2)
+				ft_putstr_fd2("error: cd: bad arguments", NULL);
+			else if(chdir(argv[1]) != 0)
+				ft_putstr_fd2("error: cd: cannot change directory to ", argv[1]);
+		} 
+		else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0))
+		{
+			pid = fork();
+			if (pid == -1)
+				ft_fatal_error();
+			if (pid == 0)
+				ft_execute(argv, i, tmp_fd, env);
+			else
+			{
+				close(tmp_fd);
+				while(waitpid(-1, NULL, 2) != -1)
+					;
+				tmp_fd = dup(0);
+				if (tmp_fd == -1)
+					ft_fatal_error();
+			}
+		}
+		else if (i != 0 && strcmp(argv[i], "|") == 0)
+		{
+			if (pipe(fd) == -1)
+				ft_fatal_error();
+			pid = fork();
+			if (pid == -1)
+				ft_fatal_error();
+			if (pid == 0)
+			{
+				if (dup2(fd[1], 1) == -1)
+					ft_fatal_error();
+				close(fd[0]);
+				close(fd[1]);
+				ft_execute(argv, i, tmp_fd, env);
+			}
+			else
+			{
+				close(fd[1]);
+				close(tmp_fd);
+				tmp_fd = fd[0];
+			}
+		}
+	}
+	close(tmp_fd);
 	return (0);
 }
